6de6df4d7fce6e96969a224d453b790f11e19806
Zhuoershang <79759438+Zhuoershang@users.noreply.github.com>
2026-05-09 15:03:04 +0800

    Update openai.ts

--- src/openai.ts
+++ src/openai.ts
@@ -455,7 +455,7 @@ function getAuthHeaders(token: string): Record<string, string> {
     "Accept-Encoding": "gzip",
     "Content-Type": "application/json",
     "x-client-platform": "android",
-    "x-client-version": "2.0.0",
+    "x-client-version": "2.0.4",
     "x-client-locale": "zh_CN",
     "accept-charset": "UTF-8",
     authorization: `Bearer ${token}`,
